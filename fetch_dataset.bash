18b81ff03a4300c61edf03cabc1f1b4c72f44a37
theSillyDinosaur <84118554+theSillyDinosaur@users.noreply.github.com>
2025-12-04 17:38:52 +0800

    Add script to fetch and download datasets

---
+++ fetch_dataset.bash
@@ -0,0 +1,3 @@
+chmod 600 /root/.kaggle/kaggle.json
+kaggle datasets download -d eduardo4jesus/stanford-cars-dataset -p datasets/car --unzip
+kaggle datasets download -d wenewone/cub2002011 -p datasets/bird --unzip
